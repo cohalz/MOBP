c683bb1c94c158680f891ba50a76bd162ae3e18e
cohalz <cohalzweb@gmail.com>
2015-11-13 14:11:00 +0900

    fix

--- config.rb
+++
@@ -1,8 +0,0 @@
-ADMIN_SCREEN_NAME = 'cohalz'
-BOT_SCREEN_NAME = 'MOBP_'
-# define twitter application parameters here
-YOUR_CONSUMER_KEY = 'jfkeKR6IGOdImndLh0XeV0sfO'
-YOUR_CONSUMER_SECRET = 'LEnXDbzkzuIhD6ySZR39GQX16t3gaIcI2FE2B6bPBchSqTMYjX'
-YOUR_OAUTH_TOKEN = '3184239678-vbm4b26b0hibDCi6XSDaA1leIsNEK4Ulve01Zgy'
-YOUR_OAUTH_TOKEN_SECRET = '9aqJBve1KVp8Qk0MjHgcg92WypHNHoGZksCKJ6qn3vEqC'
-CSV_PATH = '/home/cohalz/tweets.csv'
