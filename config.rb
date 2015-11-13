64141b9cf61baa4ff9f4bf05544184fae42049c6
cohalz <cohalzweb@gmail.com>
2015-11-13 14:35:15 +0900

    :add: config

---
+++ config.rb
@@ -0,0 +1,10 @@
+require 'rexml/document'
+
+ADMIN_SCREEN_NAME = 'cohalz'
+BOT_SCREEN_NAME = 'MOBP_'
+
+YOUR_CONSUMER_KEY = settings.elements['root/twitter/mobp/consumerKey'].text
+YOUR_CONSUMER_SECRET = settings.elements['root/twitter/mobp/consumerSecret'].text
+YOUR_OAUTH_TOKEN = settings.elements['root/twitter/mobp/accessToken'].text
+YOUR_OAUTH_TOKEN_SECRET = settings.elements['root/twitter/mobp/accessTokenSecret'].text
+DBPASSWD = settings.elements['root/db/mysql/pass'].text
